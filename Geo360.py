0a0cba8e6e3e3e35df198d126dbce2f6a910c849
Agnieszka Pawlaczek <agnieszkapawlaczek7@gmail.com>
2025-01-09 14:00:56 +0100

    Zmiana print na qgis log #23

--- Geo360.py
+++ Geo360.py
@@ -63,7 +63,7 @@ def import_exifread():
             sys.path.append(exifread_path) 
             import exifread  
         else:
-            print("Nie znaleziono lokalnej wersji 'exifread'. Proszę zainstalować bibliotekę.")
+            QgsMessageLog.logMessage("Nie znaleziono lokalnej wersji 'exifread'. Proszę zainstalować bibliotekę.", "PhotoViewer360", Qgis.Warning)
 
 import_exifread()
 
